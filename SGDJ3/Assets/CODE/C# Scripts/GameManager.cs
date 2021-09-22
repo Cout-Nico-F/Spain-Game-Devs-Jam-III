2cda907313a3b6ab2c9972ddbfb4d7a386826466
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-09-22 01:44:25 -0300

    Update GameManager.cs

--- SGDJ3/Assets/CODE/C# Scripts/GameManager.cs
+++ SGDJ3/Assets/CODE/C# Scripts/GameManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    private bool isPaused;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    IEnumerator WaitForSecondsCoroutine(float seconds)
+    {
+        //Wait for X seconds
+        yield return new WaitForSecondsRealtime(seconds);
+    }
+
+    public void ToMainMenu()
+    {
+        Time.timeScale = 1;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+    public void Pause()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            //uiManager.ShowPauseUi(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            //uiManager.ShowPauseUi(false);
+        }
+    }
+
 }
