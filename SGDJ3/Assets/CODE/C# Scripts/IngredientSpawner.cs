ec4f14229305edf5b155f7e08741ed7fc48e1532
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-09-22 17:50:50 -0300

    Ingredient Spawner

---
+++ SGDJ3/Assets/CODE/C# Scripts/IngredientSpawner.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<Ingredient> ingredients;
+    
+    private float spawnDelayInSeconds = 1;
+    private List<Ingredient> ingredientPool;
+    private float timeRemaining;
+
+
+    private void Awake()
+    {
+        timeRemaining = 3; //initial delay
+        //seteo primera vuelta de ingredientes ( aca podemos cambiarlo a antojo para que siempre toque algun ingrediente primero
+        //puede ser util para enseñar la primer pocion a modo de tutorial )
+        ingredientPool = new List<Ingredient>(ingredients);
+    }
+
+    private void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        //TODO: restricciones de spawn (ejemplo no spawn donde ya hay otro ingrediente )
+        //spawnear
+        Instantiate(ChooseIngredient(), ChoosePosition(), Quaternion.identity) ;
+        //resetear timer de cooldown
+        timeRemaining = spawnDelayInSeconds;
+    }
+
+    private Ingredient ChooseIngredient()
+    {
+        int chosenOneIndex = Random.Range(0, ingredientPool.Count);
+        Ingredient chosenOne = ingredientPool[chosenOneIndex];
+
+        ingredientPool.RemoveAt(chosenOneIndex);
+
+        if(ingredientPool.Count == 0)
+        {
+            ingredientPool = ingredients;
+        }
+
+        return chosenOne;
+    }
+
+    private Vector3 ChoosePosition()
+    {
+        //find a random X and Y values
+        int randomX = 0;
+        int randomY = 0;
+        int Zpos = 0;
+        Vector3 vec = new Vector3(randomX, randomY,Zpos);
+        return vec;
+    }
+}
