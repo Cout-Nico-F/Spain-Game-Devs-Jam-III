d13daf8eddf27b7e20c5cbdadb04b9c26050ee56
jhcanti <jhcanti@gmail.com>
2021-09-23 13:33:27 +0200

    Update DragAndDrop.cs

--- SGDJ3/Assets/CODE/C# Scripts/DragAndDrop.cs
+++ SGDJ3/Assets/CODE/C# Scripts/DragAndDrop.cs
@@ -7,6 +7,7 @@ public class DragAndDrop : MonoBehaviour
 {
     private Transform myTransform;
     private Camera cam;
+    private Vector3 offset;
     
     private void Awake()
     {
@@ -16,13 +17,14 @@ public class DragAndDrop : MonoBehaviour
 
     private void OnMouseDown()
     {
-        
+        var mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        offset = myTransform.position - mousePosition;
     }
 
     private void OnMouseDrag()
     {
         var mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        myTransform.position = new Vector3(mousePosition.x, mousePosition.y, myTransform.position.z);            
+        myTransform.position = new Vector3(mousePosition.x + offset.x, mousePosition.y + offset.y, myTransform.position.z);            
     }
 
     private void OnMouseUp()
