80e45f26c265891fdb8faf9fcee33d89f2195068
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-09-23 01:23:31 -0300

    Sprite y codigo de pociones para lanzadera

---
+++ SGDJ3/Assets/RedPotion.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedPotion : MonoBehaviour
+{
+    private bool isPressed = false;
+    private float releaseTime;
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+}
