30c21b32f6d456de1905a7ef5af8cb28ca7122f8
jhcanti <jhcanti@gmail.com>
2021-09-24 11:47:10 +0200

    UiManager to Singleton

--- SGDJ3/Assets/CODE/C# Scripts/UiManager.cs
+++ SGDJ3/Assets/CODE/C# Scripts/UiManager.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class UiManager : MonoBehaviour
+public class UiManager : Singleton<UiManager>
 {
     [SerializeField]
     GameObject pausePanel;
+    
     public void Pause()
     {
         pausePanel.SetActive(true);
     }
+    
     public void UnPause()
     {
         pausePanel.SetActive(false);
