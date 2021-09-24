5d518c2f06a3e6c42f2672e3364610f92bcff02c
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-09-24 00:21:24 -0300

    introducing friendJoined & health

--- SGDJ3/Assets/CODE/C# Scripts/LevelManager.cs
+++ SGDJ3/Assets/CODE/C# Scripts/LevelManager.cs
@@ -2,28 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : Singleton<LevelManager>
 {
     [SerializeField]
     private int levelObjective;
 
     private int friendCount;
 
+    private int health;
+    private int maxHealth = 6;
+
     public int LevelObjective { get => levelObjective; }
 
+
+    private void Awake()
+    {
+        health = maxHealth;
+    }
+
+    //Este metodo lo llamamos al detectar colision pocion-npc y comprobar que son del mismo color.
     public void FriendJoined()
     {
         friendCount++;
-    }
 
-
-    private bool CheckCompletionStatus()
-    {
         if (friendCount >= LevelObjective)
         {
-            return true;
-        }else
-            return false;
+            LevelCompleted();
+        }
     }
 
+    private void LevelCompleted()
+    {
+        //do
+    }
 }
