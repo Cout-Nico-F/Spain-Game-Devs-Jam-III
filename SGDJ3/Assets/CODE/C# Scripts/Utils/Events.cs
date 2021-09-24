be8d9c034cd9c8e50fc0100ed77f341b6d89338a
jhcanti <jhcanti@gmail.com>
2021-09-24 11:45:28 +0200

    Clase de utilidad para eventos

---
+++ SGDJ3/Assets/CODE/C# Scripts/Utils/Events.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine.Events;
+
+public class Events
+{
+    [System.Serializable] public class EventLevelComplete : UnityEvent<bool> { }
+    [System.Serializable] public class EventGameOver : UnityEvent<bool> { }
+}
