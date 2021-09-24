53909cd9adc341f21d09bbf978ee8a20c3834fa2
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-09-24 00:16:57 -0300

    Npc prefabs

---
+++ SGDJ3/Assets/CODE/C# Scripts/Npc.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : MonoBehaviour
+{
+    [SerializeField]
+    private State _state;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Potion"))
+        {
+            if (collision.GetComponent<Potion>().color.Equals(_state.color))
+            {
+                LevelManager.Instance.FriendJoined();
+                Debug.Log("Cured, he wants to join the party!");
+
+            }
+            else
+            {
+                Debug.Log("Exploding recipe.");
+            }
+        }
+    }
+}
