b0a0bf3d07c5ff63d1b2fa8b3e3d72b8af934116
jhcanti <jhcanti@gmail.com>
2021-09-25 17:24:46 +0200

    nuevo script para gestionar la vida de la bruja

---
+++ SGDJ3/Assets/CODE/C# Scripts/Npc/Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private Sprite[] images;
+    
+    private Image[] hearts;
+    private int heartNumber;
+    private bool fullHeart;
+
+
+    private void Awake()
+    {
+        hearts = GetComponentsInChildren<Image>();
+        heartNumber = 2;
+        fullHeart = true;
+    }
+
+    
+    public void TakeDamage()
+    {
+        if (fullHeart)
+        {
+            hearts[heartNumber].sprite = images[1];
+        }
+        else
+        {
+            hearts[heartNumber].sprite = null;
+            var color = hearts[heartNumber].color;
+            color.a = 0;
+            hearts[heartNumber].color = color;
+            heartNumber--;
+            if (heartNumber < 0)
+            {
+                Debug.Log("Level Over");
+                return;
+            }
+        }
+        fullHeart = !fullHeart;
+    }
+}
