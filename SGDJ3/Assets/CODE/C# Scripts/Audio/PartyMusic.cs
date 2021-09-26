6fe6543005efe200518bcfcbd81a1c60435e2ae9
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-09-26 19:33:44 -0300

    music scrpit Party

---
+++ SGDJ3/Assets/CODE/C# Scripts/Audio/PartyMusic.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AudioSystem.Instance.Play("Party Music");
+    }
+}
